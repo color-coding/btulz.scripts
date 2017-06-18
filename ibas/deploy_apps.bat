6e50af4d798cec22e5da334da5d056d0d3de5f33
牛加人等于朱 <niuren.zhu@icloud.com>
2017-06-18 16:50:51 +0800

    完善部署脚本。

--- ibas/deploy_apps.bat
+++ ibas/deploy_apps.bat
@@ -1,17 +1,17 @@
 @echo off
 setlocal EnableDelayedExpansion
 echo *************************************************************************************
-echo             deploy.bat
+echo             deploy_apps.bat
 echo                  by niuren.zhu
 echo                          2017.06.18
 echo  说明：
-echo     1. 下载并部署IBAS的WAR到部署目录，需要以管理员权限启动。
+echo     1. 部署IBAS的WAR包到应用目录，需要以管理员权限启动。
 echo     2. 参数1，IBCP数据目录，默认.\ibas。
 echo     3. 参数2，IBAS的包位置，默认.\ibas_packages。
 echo     4. 参数3，IBAS部署目录，默认.\webapps。
 echo     5. 参数4，IBAS共享库目录，默认.\ibas_lib。
 echo     6. 脚本通文件链接方式，集中配置文件和日志目录到IBAS_HOME下。
-echo     7. 提前下载并配置wget、7z到PATH变量。
+echo     7. 提前下载并配置7z到PATH变量，或拷贝到.\ibas_tools目录。
 echo     8. 请调整conf\catalina.properties的shared.loader="${catalina.home}/ibas_lib/*.jar"。
 echo **************************************************************************************
 SET h=%time:~0,2%
@@ -45,9 +45,18 @@ if "%IBAS_LIB%" equ "" SET IBAS_LIB=%WORK_FOLDER%ibas_lib\
 if not exist "%IBAS_LIB%" mkdir "%IBAS_LIB%"
 REM 设置7z
 SET TOOL_7Z=%WORK_FOLDER%ibas_tools\7z.exe
+if not exist "%TOOL_7Z%" SET TOOL_7Z=7z.exe
 REM 设置备份目录
 SET IBAS_PACKAGE_BACKUP=%IBAS_PACKAGE%backup\%DATE_NAME%\
 if not exist "%IBAS_PACKAGE_BACKUP%" mkdir "%IBAS_PACKAGE_BACKUP%"
+
+REM 显示参数信息
+echo ----------------------------------------------------
+echo 应用包目录：%IBAS_PACKAGE%
+echo 部署目录：%IBAS_DEPLOY%
+echo 共享目录：%IBAS_LIB%
+echo 数据目录：%IBAS_HOME%
+echo ----------------------------------------------------
 
 
 echo 开始解压[%IBAS_PACKAGE%]的war包
@@ -94,6 +103,8 @@ REM 备份程序包
         move "%IBAS_PACKAGE%%%m" "%IBAS_PACKAGE_BACKUP%%%m"
     )
 )
+REM 备份顺序文件
+move "%IBAS_PACKAGE%ibas.deploy.order.txt" "%IBAS_PACKAGE_BACKUP%ibas.deploy.order.txt"
 REM 修正ROOT目录
 if exist "%IBAS_DEPLOY%root" rename "%IBAS_DEPLOY%root" ROOT
 echo 操作完成
