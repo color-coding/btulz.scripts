6cfea6b754ee2066a4b91b44560fdc4dce7a0ac3
niurenzhu <niuren.zhu@icloud.com>
2017-09-19 21:04:50 +0800

    判断脚本是否存在

--- ibas/compiles.sh
+++ ibas/compiles.sh
@@ -3,9 +3,9 @@ echo '**************************************************************************
 echo '              compiles.sh                                                   '
 echo '                      by niuren.zhu                                         '
 echo '                           2017.06.01                                       '
-echo '  说明：                                                                    '
-echo '    1. 遍历工作目录，存在compile_packages.bat则调用。                       '
-echo '    2. 参数1，工作目录。                                                    '
+echo '  说明：                                                                     '
+echo '    1. 遍历工作目录，存在compile_packages.bat则调用。                           '
+echo '    2. 参数1，工作目录。                                                       '
 echo '****************************************************************************'
 # 设置参数变量
 # 启动目录
@@ -32,12 +32,15 @@ fi
 # 遍历当前目录存
 while read folder
 do
-  if [ ! -x "${WORK_FOLDER}/${folder}/compile_packages.sh" ]
+  if [ -e ${WORK_FOLDER}/${folder}/compile_packages.sh ]
   then
-    chmod 775 ${WORK_FOLDER}/${folder}/compile_packages.sh
+    if [ ! -x ${WORK_FOLDER}/${folder}/compile_packages.sh ]
+    then
+      chmod 775 ${WORK_FOLDER}/${folder}/compile_packages.sh
+    fi
+    cd ${WORK_FOLDER}/${folder}
+    echo ----开始编译：`pwd`
+    ${WORK_FOLDER}/${folder}/compile_packages.sh
   fi
-  cd ${WORK_FOLDER}/${folder}
-  echo ----开始编译：`pwd`
-  "${WORK_FOLDER}/${folder}/compile_packages.sh" 
 done < ${WORK_FOLDER}/compile_order.txt | sed 's/\r//g'
 cd ${WORK_FOLDER}
