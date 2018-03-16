2e9bb6050f9837aa0e31d63bff31f8e4f85c3b82
牛加人等于朱 <niuren.zhu@icloud.com>
2018-03-16 11:38:04 +0800

    添加压缩js命令

--- ibas/builds.sh
+++ ibas/builds.sh
@@ -5,7 +5,8 @@ echo '                      by niuren.zhu                                       
 echo '                           2017.06.01                                       '
 echo '  说明：                                                                    '
 echo '    1. 遍历工作目录，存在build_all.bat则调用。                              '
-echo '    2. 参数1，工作目录。                                                    '
+echo '    2. 使用uglifyjs压缩*.js文件为*.min.js。                                 '
+echo '    3. 参数1，工作目录。                                                    '
 echo '****************************************************************************'
 # 设置参数变量
 # 启动目录
@@ -24,6 +25,16 @@ then
 fi
 
 echo --工作的目录：${WORK_FOLDER}
+echo --检查工具
+COMPRESS=false
+uglifyjs -V
+if [ "$?" = "0" ]
+then
+  COMPRESS=true
+else
+  echo 请先安装压缩工具[npm install uglify-es -g]
+fi
+
 # 获取编译顺序
 if [ ! -e ${WORK_FOLDER}/compile_order.txt ]
 then
@@ -41,7 +52,20 @@ do
     fi
     echo --开始调用：${builder}
     "${builder}"
-    echo '****************************************************************************'
   done
+# 尝试压缩js文件
+  if [ "${COMPRESS}" = "true" ]
+  then
+# 遍历当前目录
+    for file in `find ${WORK_FOLDER}/${folder} -type f -name *.js ! -name *.min.js ! -path "*3rdparty*" ! -path "*openui5/resources*" ! -path "*target*"`
+    do
+      compressed=${file%%.js*}.min.js
+      echo --开始压缩：${file}
+      uglifyjs --compress --keep-classnames --keep-fnames --mangle --output ${compressed} ${file}
+    done
+  fi
+  echo '****************************************************************************'
 done < ${WORK_FOLDER}/compile_order.txt | sed 's/\r//g'
 cd ${WORK_FOLDER}
+
+
