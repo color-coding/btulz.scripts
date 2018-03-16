bbe62e278a17bd99c0d0e0acbe0b238bee61ff58
牛加人等于朱 <niuren.zhu@icloud.com>
2018-03-16 18:05:03 +0800

    添加压缩js命令

--- ibas/builds.bat
+++ ibas/builds.bat
@@ -18,18 +18,47 @@ if "%WORK_FOLDER%"=="" SET WORK_FOLDER=%STARTUP_FOLDER%
 REM 若工作目录最后字符不是“\”则补齐
 if "%WORK_FOLDER:~-1%" neq "\" SET WORK_FOLDER=%WORK_FOLDER%\
 
+echo --工作的目录：%WORK_FOLDER%
+echo --检查工具
+SET COMPRESS=false
+call uglifyjs -V
+if "%ERRORLEVEL%"=="0" (
+  SET COMPRESS=true
+) else (
+  echo 请先安装压缩工具[npm install uglify-es -g]
+)
+
 if not exist "%WORK_FOLDER%compile_order.txt" dir /a:d /b "%WORK_FOLDER%" >"%WORK_FOLDER%compile_order.txt"
 
-echo --工作的目录：%WORK_FOLDER%
 for /f %%l in (%WORK_FOLDER%compile_order.txt) do (
   set FOLDER=%%l
   for /f %%m in ('dir /s /b "%WORK_FOLDER%!FOLDER!\*build_all.bat"') DO (
-    SET FOLDER=%%~pm
     SET BUILDER=%%m
-    cd /d !FOLDER!
+    cd /d %%~pm
     echo --开始调用：!BUILDER!
     call !BUILDER!
-    echo ****************************************************************************
+    cd /d !WORK_FOLDER!
   )
+REM 尝试压缩js文件
+  if "%COMPRESS%"=="true" (
+REM 遍历当前目录
+    for /f %%n in ('dir /s /b %WORK_FOLDER%!FOLDER!\*.js') DO (
+      set FILE=%%n
+      set DONE=true
+      set TMP_VALUE=!FILE!
+      if "!DONE!"=="true" if "!TMP_VALUE:target=!"=="!TMP_VALUE!" (set DONE=true) else set DONE=false
+      set TMP_VALUE=!FILE!
+      if "!DONE!"=="true" if "!TMP_VALUE:3rdparty=!"=="!TMP_VALUE!" (set DONE=true) else set DONE=false
+      set TMP_VALUE=!FILE!
+      if "!DONE!"=="true" if "!TMP_VALUE:openui5\resources=!"=="!TMP_VALUE!" (set DONE=true) else set DONE=false
+      set TMP_VALUE=!FILE!
+      if "!DONE!"=="true" if "!TMP_VALUE:~-7!" neq ".min.js" (set DONE=true) else set DONE=false
+      if "!DONE!"=="true" (
+        set COMPRESSED=!FILE:~0,-3!.min.js
+        echo --开始压缩：!FILE!
+        call uglifyjs --compress --keep-classnames --keep-fnames --mangle --output !COMPRESSED! !FILE!
+      )
+    )
+  )
+  echo ****************************************************************************
 )
-cd /d %WORK_FOLDER%
